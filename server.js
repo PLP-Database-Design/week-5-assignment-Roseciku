125e42aa749e24c925075853df185ba2bf824330
Rosemary Wangui <rosecikumary@gmail.com>
2024-09-29 21:17:41 +0300

    added .ejs

--- server.js
+++ server.js
@@ -20,58 +20,61 @@ db.connect((err)=>{
     console.log('Successfully connected to mysql:', db.threadId)
 })
 
+app.set('view engine', 'ejs');
+app.set('views', __dirname + '/views');
+
 // question 1
 
-app.get('', (req, res)=> {
+app.get('/patients', (req, res)=> {
   
     const getPatients = "SELECT patient_id, first_name, last_name, date_of_birth FROM patients"
     db.query(getPatients, (err,data) =>{
         if(err){
             return res.status(400).send('Failed to get patients', err)
         }
-        res.status(200).send(data)
+        res.status(200).render('patients',{data})
     })
     
     })
 
 //question 2
 
-    app.get('',(req, res)=>{
+    app.get('/providers',(req, res)=>{
 
         const getProvider = "SELECT first_name, last_name, provider_specialty FROM providers"
         db.query(getProvider, (err,data) =>{
             if(err){
                 return res.status(400).send('Failed to get providers', err)
             }
-            res.status(200).send(data)
+            res.status(200).render('providers',{data})
     })
 
 
 })
 
-//Question 3
+// //Question 3
 
-app.get('',(req, res)=>{
+app.get('/patients/firstname',(req, res)=>{
     const getPatientFirstName = "SELECT first_name FROM patients"
     db.query(getPatientFirstName, (err,data) =>{
         if(err){
             return res.status(400).send('Failed to get patient first name', err)
         }
-        res.status(200).send(data)
+        res.status(200).render('patients',{data})
 })
 
 
 })
 
-//Question 4
+// //Question 4
 
-    app.get('',(req, res)=>{
+    app.get('/providers/specialty',(req, res)=>{
         const getProviderSpecialty = "SELECT first_name, last_name, provider_specialty FROM providers"
         db.query(getProviderSpecialty, (err,data) =>{
             if(err){
                 return res.status(400).send('Failed to get providers', err)
             }
-            res.status(200).send(data)
+            res.status(200).render('providers',{data})
     })
 })
 
